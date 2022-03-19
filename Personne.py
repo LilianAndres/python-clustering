49cce8826b2d5eaa40ab646ac3248438bd3a472b
Lilian Andres <lilian.andres@etu.univ-lyon1.fr>
2022-03-19 21:01:01 +0100

    Normalisation des données (1 ligne à debug)

--- Personne.py
+++ Personne.py
@@ -1,25 +1,42 @@
 from itertools import count
 
+from numpy import char
+
 
 class Personne:
     
     _id = count(0)
 
-    def __init__(self, nom, marqueTel, jeux, isContamined, taille, projetPro, possedeStage, moisNaissance, possedePermis):
+    def __init__(self, nom, tempsTrajet, marqueTelephone, charismatique, gamer, taille, projetPro, stage, anneeNaissance, navigateur, os, holeio, covid, dylan, longueurCheveux, moisNaissance, fraternite, couleurYeux, couleurCheveux, permis):
+        # identification
         self.id = next(self._id)
         self.nom = nom
-        self.marqueTel = marqueTel
-        self.jeux = jeux
-        self.isContamined = isContamined
-        self.taille = taille
+        # métriques principales
+        self.tempsTrajet = tempsTrajet
+        self.marqueTelephone = marqueTelephone
+        self.charismatique = charismatique
+        self.gamer = gamer
+        self.taille = taille 
         self.projetPro = projetPro
-        self.possedeStage = possedeStage
+        self.stage = stage
+        self.anneeNaissance = anneeNaissance
+        self.navigateur = navigateur
+        self.os = os
+        # sous-métriques
+        self.holeio = holeio
+        self.covid = covid
+        self.dylan = dylan
+        self.longueurCheveux = longueurCheveux
         self.moisNaissance = moisNaissance
-        self.possedePermis = possedePermis
+        self.fraternite = fraternite
+        self.couleurYeux = couleurYeux
+        self.couleurCheveux = couleurCheveux
+        self.permis = permis
+        
 
 
     def Comparer(self, personne):
-        """Compare deux personnes en fonction de leurs métriques"""
+        """Compare deux personnes en fonction de leurs métriques et de leurs sous-métriques"""
 
         compatibilite = 0
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas
 from Maillon import Maillon
@@ -7,38 +8,88 @@ from Struct import Struct
 def main():
     
     dataset = pandas.read_csv("dataset.csv")
+
+    # normalisation des données
+    dataset.drop(dataset.columns[[2,5,6,7,8,10,12,14,15,17]], axis=1, inplace=True)
+    dataset.rename(columns={
+        "Duree du trajet": "temps_trajet",
+        "Aimez vous Hole-io ?": "hole_io",
+        "Marque téléphone": "telephone",
+        "Etes-vous viril (e)": "charismatique",
+        "Êtes-vous un gamer ?": "gamer",
+        "Avez-vous déjà eu le covid19 ?": "covid",
+        "Est-ce que Dylan est une mauvaise personne ?": "dylan", # débug !
+        "Taille ?": "taille",
+        "Longueur de vos cheveux en cm": "longueur_cheveux",
+        "Projet Post-DUT": "projet_pro",
+        "Avez-vous un stage ?": "stage",
+        "Mois de naissance": "mois_naissance",
+        "Année de naissance": "annee_naissance",
+        "Navigateur web": "navigateur",
+        "Etes-vous fils/fille unique ?": "fraternite",
+        "Couleur des yeux": "couleur_yeux",
+        "Quel système d'exploitation ?": "os",
+        "Couleur des cheveux": "couleur_cheveux",
+        "Avez-vous le permis de conduire ?": "permis",
+        "Qel est votre nom": "nom"
+    }, inplace=True)
+
+    print(dataset.columns)
     
-    struct = Struct()
+#     struct = Struct()
 
-    # initialise le graphe avec des 0 
-    grapheInteraction = np.zeros((len(dataset), len(dataset))) 
+#     # initialise le graphe avec des 0 
+#     grapheInteraction = np.zeros((len(dataset), len(dataset))) 
 
-   # créer la structure avec tous les maillons
-    for i in range(len(dataset)):
-        personne = Personne(dataset.iloc[i, 29], dataset.iloc[i, 4], dataset.iloc[i, 3], dataset.iloc[i, 12],
-        dataset.iloc[i, 16], dataset.iloc[i, 19], dataset.iloc[i, 20], dataset.iloc[i, 21], dataset.iloc[i, 27])
-        struct.Ajouter(personne)
+
+#    # créer la structure avec tous les maillons
+#     for i in range(len(dataset)):
+
+#         personne = Personne(
+#             nom=dataset[['nom']].iloc[i],
+#             tempsTrajet=dataset[['temps_trajet']].iloc[i],
+#             marqueTelephone=dataset[['telephone']].iloc[i],
+#             charismatique=dataset[['charismatique']].iloc[i],
+#             gamer=dataset[['gamer']].iloc[i],
+#             taille=dataset[['taille']].iloc[i],
+#             projetPro=dataset[['projet_pro']].iloc[i],
+#             stage=dataset[['stage']].iloc[i],
+#             anneeNaissance=dataset[['annee_naissance']].iloc[i],
+#             navigateur=dataset[['navigateur']].iloc[i],
+#             os=dataset[['os']].iloc[i],
+#             holeio=dataset[['hole_io']].iloc[i],
+#             covid=dataset[['covid']].iloc[i],
+#             dylan=dataset[['dylan']].iloc[i],
+#             longueurCheveux=dataset[['longueur_cheveux']].iloc[i],
+#             moisNaissance=dataset[['mois_naissance']].iloc[i],
+#             fraternite=dataset[['fraternite']].iloc[i],
+#             couleurYeux=dataset[['couleur_yeux']].iloc[i],
+#             couleurCheveux=dataset[['couleur_cheveux']].iloc[i],
+#             permis=dataset[['permis']].iloc[i]
+#         )
+
+#         struct.Ajouter(personne)
 
     
-    ptr = struct.tete
+#     ptr = struct.tete
 
-    # remplir les voisins dans chaque maillon (liste des personnes incompatibles)
-    while ptr.suivant != None:
-        ptr2 = struct.tete
-        while ptr2.suivant != None:
-            if ptr.p.Comparer(ptr2.p) == False:
-                ptr.voisins.append(ptr2.p)
-            ptr2 = ptr2.suivant
-        ptr = ptr.suivant
+#     # remplir les voisins dans chaque maillon (liste des personnes incompatibles)
+#     while ptr.suivant != None:
+#         ptr2 = struct.tete
+#         while ptr2.suivant != None:
+#             if ptr.p.Comparer(ptr2.p) == False:
+#                 ptr.voisins.append(ptr2.p)
+#             ptr2 = ptr2.suivant
+#         ptr = ptr.suivant
         
-    ptr = struct.tete
+#     ptr = struct.tete
 
-    # remplir le graphe d'interaction avec les voisins 
-    while ptr.suivant != None:
-        # pour chaque ligne de la matrice
-        for voisin in ptr.voisins:
-            grapheInteraction[ptr.p.id][voisin.id] = 1
-        ptr = ptr.suivant
+#     # remplir le graphe d'interaction avec les voisins 
+#     while ptr.suivant != None:
+#         # pour chaque ligne de la matrice
+#         for voisin in ptr.voisins:
+#             grapheInteraction[ptr.p.id][voisin.id] = 1
+#         ptr = ptr.suivant
 
     
     # colorer le graphe d'interaction
